6037f7571b479198c531d5e9899319f4a8a1f68b
drelocatelli <andressalocatelli@hotmail.com>
2026-07-27 15:52:38 -0300

    retira DEBUg

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -15,7 +15,7 @@ const {
   getItemsLevel,
 } = require('./levels.js');
 
-const DEBUG = true;
+const DEBUG = false;
 const SAVE_FILE = path.join(__dirname, 'saves', 'player_saves.json');
 
 const app = express();
